6c48557a5a9647ca6ec3c752b614e9099f7674bc
xiaoqiang <1341974376@qq.com>
2020-03-24 17:59:28 +0800

    1.create project.

---
+++ ZKPalmSDKdemo/app/src/main/java/com/zkteco/zkpalmsdkdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zkteco.zkpalmsdkdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Administrator
+ * Created by：Administrator on 2020/3/24 17:57
+ * Email：qiang.xiao@zkteco.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
